1f64eac4b4cce010006818e82af1e3b9e649b129
krishnaTech6 <krishna060702@gmail.com>
2024-10-10 20:41:48 +0530

    added firebase , webrtc dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -36,7 +39,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -45,4 +47,12 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(platform(libs.firebase.bom))
+    implementation (libs.firebase.database.ktx)
+    implementation (libs.gson)
+    implementation (libs.webrtc)
+    implementation (libs.dagger.hilt.android)
+    kapt(libs.hilt.compiler)
+    implementation (libs.permissionx)
 }
