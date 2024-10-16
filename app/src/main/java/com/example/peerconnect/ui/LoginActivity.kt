c1dbdb753646463558b35f7702a343d3183a121a
krishnaTech6 <krishna060702@gmail.com>
2024-10-16 15:58:21 +0530

    Added applicationclass, login ui , colors

--- app/src/main/java/com/example/peerconnect/ui/LoginActivity.kt
+++ app/src/main/java/com/example/peerconnect/ui/LoginActivity.kt
@@ -1,17 +1,18 @@
-package com.example.peerconnect
+package com.example.peerconnect.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.peerconnect.R
 import com.google.firebase.database.FirebaseDatabase
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
