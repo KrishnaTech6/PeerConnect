ae37a19fec20b052690612b620da452b76442bb6
krishnaTech6 <krishna060702@gmail.com>
2024-10-18 18:38:23 +0530

    ui setup according to call type done

--- app/src/main/java/com/example/peerconnect/ui/CallActivity.kt
+++ app/src/main/java/com/example/peerconnect/ui/CallActivity.kt
@@ -5,17 +5,42 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import com.example.peerconnect.R
+import com.example.peerconnect.databinding.ActivityCallBinding
 
 class CallActivity : AppCompatActivity() {
+    private var isCaller:Boolean = true
+    private var isVideoCall: Boolean = true
+    private var target:String? = null
+    private lateinit var binding: ActivityCallBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_call)
+        binding= ActivityCallBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        init()
+    }
+
+    private fun init() {
+        intent.getStringExtra("target")?.let {
+            this.target = it
+        } ?: kotlin.run { finish() }
+        isCaller = intent.getBooleanExtra("isCaller", true)
+        isVideoCall = intent.getBooleanExtra("isVideoCall", true)
+
+        binding.apply {
+            if(!isVideoCall){
+                toggleCameraButton.isVisible= false
+                screenShareButton.isVisible= false
+                switchCameraButton.isVisible= false
+            }
         }
     }
 }
--- app/src/main/java/com/example/peerconnect/ui/MainActivity.kt
+++ app/src/main/java/com/example/peerconnect/ui/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.peerconnect.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -82,7 +82,11 @@ class MainActivity : AppCompatActivity(), MainRecyclerViewAdapter.Listener, Main
             mainRepository.sendConnectionRequest(username, true){
                 //We have to start video call
                 // move to call activity
-                Toast.makeText(this@MainActivity, "Moving to VC Activity", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this@MainActivity, CallActivity::class.java).apply {
+                    putExtra("isCaller", true)
+                    putExtra("isVideoCall", true)
+                    putExtra("target", username)
+                })
             }
         }
     }
@@ -93,6 +97,11 @@ class MainActivity : AppCompatActivity(), MainRecyclerViewAdapter.Listener, Main
                 if(it){
                     //We have to start audio call
                     // move to call activity
+                    startActivity(Intent(this@MainActivity, CallActivity::class.java).apply {
+                        putExtra("isCaller", true)
+                        putExtra("isVideoCall", false)
+                        putExtra("target", username)
+                    })
                 }
             }
         }
@@ -114,7 +123,11 @@ class MainActivity : AppCompatActivity(), MainRecyclerViewAdapter.Listener, Main
                         incomingCallLayout.isVisible= false
                         //We have to start video call
                         // move to call activity
-                        Toast.makeText(this@MainActivity, "Moving to VC Activity", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this@MainActivity, CallActivity::class.java).apply {
+                            putExtra("isCaller", false)
+                            putExtra("isVideoCall", isVideoCall)
+                            putExtra("target", model.sender)
+                        })
                     }
                 }
                 declineButton.setOnClickListener {
